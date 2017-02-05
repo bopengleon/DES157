d220e0657e60b3af1b40d7ef158ce9732ca551fe
bopengleon <bopengPRO@BoPengs-MacBook-Pro.local>
2017-02-05 02:59:23 -0800

    feb 5

---
+++ studio3/script.js
@@ -0,0 +1,20 @@
+var point1 =document.getElementById('Oval-2');
+// var point2 =document.getElementById('point2');
+// var point3 =document.getElementById('point3');
+// var point4 =document.getElementById('point4');
+// var point5 =document.getElementById('point5');
+
+var div1=document.getElementById('1');
+var div2=document.getElementById('2');
+var div3=document.getElementById('3');
+var div4=document.getElementById('4');
+var div5=document.getElementById('5');
+
+point1.addEventListener('mouseover', function(){
+div1.style.background=rgba(0,0,0,1);;
+});
+
+
+point1.addEventListener('mouseout', function(){
+
+});
