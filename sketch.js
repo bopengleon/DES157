3a5e57f90403d0426f3f068c5673f441b8a1f3d4
bopengleon <bopengPRO@BoPengs-MacBook-Pro.local>
2017-02-14 03:47:29 -0800

    feb 14

--- sketch.js
+++ sketch.js
@@ -6,14 +6,14 @@ function setup() {
     var myCanvas = createCanvas(800, 250);
     //has to parent to an id 
     myCanvas.parent('mySketch');
-     background(244, 66, 83);
+     background(66, 66, 66);
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(244, 66, 83);
+    fill(66, 66, 66);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  rect(mouseX, mouseY, 80, 80);
 }
