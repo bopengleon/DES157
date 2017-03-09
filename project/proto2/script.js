9ff407268d29ac5462925679d19708f2cae6a237
bopengleon <bopengPRO@campus-107-221.ucdavis.edu>
2017-03-09 09:31:44 -0800

    Mar 9

---
+++ project/proto2/script.js
@@ -0,0 +1,39 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGVuZzA3MjkiLCJhIjoiY2l6enQ3NGluMDQ2bDJ3bmhmMGNmZXk4YyJ9.qCYFD8QwGIzB8v5UY5Ms1Q';
+var map = new mapboxgl.Map({
+    container: 'map',
+    center: [120.147274, 30.247187],
+    zoom:13,
+    style: 'mapbox://styles/peng0729/cizzvudo2005t2sob8vjfg0d9'
+});
+
+map.on('load', function () {
+
+    map.addLayer({
+        "id": "points",
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [120.147274, 30.247187]
+                    },
+                    "properties": {
+                        "title": "WestLake",
+                        "icon": "monument"
+                    }
+                }]
+            }
+        },
+        "layout": {
+            "icon-image": "{icon}-15",
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+    });
+});
